364bb604c1f80de85665832c52412bf022915956
Ofwonojohn <Ofwonojohn004@gmail.com>
2026-03-12 12:00:21 +0300

    initial commit for the construction process tracker

---
+++ progress_track/src/main/java/track/com/progress_track/ProgressTrackApplication.java
@@ -0,0 +1,13 @@
+package track.com.progress_track;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgressTrackApplication.class, args);
+	}
+
+}
---
+++ progress_track/src/test/java/track/com/progress_track/ProgressTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package track.com.progress_track;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
